9563e6b992b0cb5edf6131a553a80129eff54a01
Rodo Talibs <rhodlib@gmail.com>
2024-01-19 20:16:56 -0300

    uuid lib, dtos, get y post

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from "./tasks.service";
+import { CreateTaskDTO } from "./dto/task.dto";
 
 @Controller('tasks')
 export class TasksController {
+    
+    constructor(private tasksService: TasksService){}
 
     @Get()
-    helloworld(){
-        return "Hello World"
+    getAllTasks(){
+        return this.tasksService.getAllTasks()
+    }
+
+    @Post()
+    createTask(@Body() newTask: CreateTaskDTO){
+        return this.tasksService.createTask(newTask.title, newTask.description)
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from "./task.entity"
+import { v4 } from "uuid";
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    tasks: Task[] = [{
+        id: "1",
+        title: "First Task",
+        description: "This is a description",
+        status: TaskStatus.DONE
+    }]
+
+    getAllTasks(){
+        return this.tasks
+    }
+
+    createTask(title: string, description: string){
+        const task = {
+            id: v4(),
+            title,
+            description,
+            status: TaskStatus.PENDING
+        }
+        this.tasks.push(task)
+        return task;
+    }
+    updateTask(){}
+    deleteTask(){}
+}
